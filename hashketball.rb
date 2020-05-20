3c6514306a6506e19e2a14a4a1f097b6bf85c2ae
Kaleb Gezahegn <kaleb.gezahegn@yale.edu>
2020-05-20 22:51:22 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -80,15 +80,15 @@ def game_hash
         slam_dunks: 2
       },
       {
-        player_name: "Reggie Evans",
+        player_name: "Bismack Biyombo",
         number: 0,
-        shoe: 14,
+        shoe: 16,
         points: 12,
-        rebounds: 12,
-        assists: 12,
-        steals: 12,
-        blocks: 12,
-        slam_dunks: 7
+        rebounds: 4,
+        assists: 7,
+        steals: 22,
+        blocks: 15,
+        slam_dunks: 10
       },
       {
         player_name: "Brook Lopez",
